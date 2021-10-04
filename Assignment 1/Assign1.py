d2f9ec31cdb4e02b9de8ad960a30aaa13203f37b
kaiz <40345089+KaizNG@users.noreply.github.com>
2021-10-04 00:30:58 -0400

    Update Assign1.py

--- Assignment 1/Assign1.py
+++ Assignment 1/Assign1.py
@@ -1,19 +1,20 @@
 import sys, time, os
+from typing import final
 
 #Clears the previous code output if any.
 os.system('cls' if os.name == 'nt' else 'clear')
 
 #Peak Hour Prices
 OFFPEAK = 0.085
-PEAK = 0.176
+ONPEAK = 0.176
 MIDPEAK = 0.119
 
 #Discounts
-TotalUsageDiscount = 0.04
-OnPeakDiscount = 0.05
-SeniorDiscount = 0.11
+TotalUsageDiscount = 0.96
+OnPeakDiscount = 0.95
+SeniorDiscount = 0.89
 
-TAX = 0.13
+TAX = 1.13
 
 def inputRestriction():
   while True:
@@ -26,7 +27,7 @@ def inputRestriction():
       continue
 
     if a < 0:
-      print(f"Please enter a positive number\n")
+      print("Please enter a positive number\n")
     else:
       break
   return a
@@ -45,29 +46,45 @@ def seniorCheck():
   else:
     print("Please try again.")
     seniorCheck()
-
-
-
+  
 def main():
   offPeakInput = -1
   onPeakInput = -1
   midPeakInput = -1
   while (offPeakInput != 0):
-    print("Enter kwh during Off Peak period: ")
-    offPeakInput = inputRestriction()
+    #print("Enter kwh during Off Peak period: ")
+    offPeakInput = float(input("Enter kwh during Off Peak period: "))
     if (offPeakInput == 0):
       print("Process finished with exit code 0")
       break
 
-    print("Enter kwh during On Peak period: ")
-    onPeakInput = inputRestriction()
+    #print("Enter kwh during On Peak period: ")
+    onPeakInput = float(input("Enter kwh during On Peak period: "))
 
-    print("Enter kwh during Mid Peak period: ")
-    midPeakInput = inputRestriction()
+    #print("Enter kwh during Mid Peak period: ")
+    midPeakInput = float(input("Enter kwh during Mid Peak period: "))
 
     seniorCheck()
     break
+  
+  onPeakPrice = onPeakInput * ONPEAK
+  offPeakPrice = offPeakInput * OFFPEAK
+  midPeakPrice = midPeakInput * MIDPEAK
+  preFinalPrice = onPeakPrice + offPeakPrice + midPeakPrice
 
-  print(offPeakInput,  onPeakInput, midPeakInput)#, seniorCheck.areYouSenior)
+  totalUsage = offPeakInput + onPeakInput + midPeakInput
+  if (totalUsage < 400) and (onPeakInput > 150):
+    preFinalPrice = preFinalPrice * TotalUsageDiscount
+    
+  elif (onPeakInput < 150):
+    onPeakPrice *= OnPeakDiscount
+
+  if (seniorCheck.areYouSenior == "y"):
+    preFinalPrice *= SeniorDiscount
+
+  print("Electricity cost: ${}".format(finalPrice))
+
+  #print(offPeakInput,  onPeakInput, midPeakInput)#, seniorCheck.areYouSenior)
+
 
 main()
